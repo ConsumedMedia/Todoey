a122016f59398b1fa29f2cefd73ccde931e06401
Nathan Schoener <nathan@consumedmedia.net>
2019-06-07 21:27:54 -0400

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Nathan Schoener on 6/7/19.
+//  Copyright © 2019 Nathan Schoener. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
